89903d969a375eba6b9bd506d3679e396872f63d
Neural-Ai <108761643+Neural-Ai@users.noreply.github.com>
2023-10-03 16:02:04 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import { Component, createResource, onMount } from 'solid-js';
+import { createEffect, createSignal, onCleanup } from 'solid-js';
+import AgGridSolid, { AgGridSolidRef } from 'ag-grid-solid';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+import './App.css';
+
+import 'ag-grid-enterprise';
+
+const MyRenderer = (props: any) => {
+  return (
+    <span class="my-renderer">
+      <img
+        src="https://www.ag-grid.com/example-assets/spinner.gif"
+        class="my-spinner"
+      />
+      <span class="my-renderer-value">{props.value}</span>
+    </span>
+  );
+};
+
+const fetchData = async () =>
+  (
+    await fetch(`https://www.ag-grid.com/example-assets/olympic-winners.json`)
+  ).json();
+
+const App: Component = () => {
+  const [rowData] = createResource<any[]>(fetchData);
+
+  let gridRef: AgGridSolidRef;
+
+  // show chart of first rendering
+  const onFirstDataRendered = () => {
+    gridRef.api.createRangeChart({
+      chartType: 'groupedColumn',
+      cellRange: {
+        rowStartIndex: 0,
+        rowEndIndex: 4,
+        columns: ['ag-Grid-AutoColumn', 'gold', 'silver'],
+      },
+    });
+  };
+
+  const columnDefs = [
+    {
+      field: 'country',
+      enableRowGroup: true,
+      rowGroup: true,
+      hide: true,
+      cellRenderer: MyRenderer,
+    },
+    { field: 'sport', enableRowGroup: true, hide: true, rowGroup: true },
+    { field: 'athlete', enableRowGroup: true, hide: true },
+    { field: 'gold', aggFunc: 'sum' },
+    { field: 'silver', aggFunc: 'sum' },
+    { field: 'bronze', aggFunc: 'sum' },
+    { field: 'total', aggFunc: 'sum' },
+  ];
+
+  const defaultColDef = {
+    resizable: true,
+    sortable: true,
+  };
+  const autoGroupColumnDef = {
+    cellRendererParams: {
+      suppressCount: true,
+      checkbox: true,
+    },
+    field: 'athlete',
+    width: 300,
+  };
+
+  return (
+    <div
+      style={{ height: '100%', display: 'flex', 'flex-direction': 'column' }}
+    >
+      <div class="ag-theme-alpine" style={{ 'flex-grow': 1 }}>
+        <AgGridSolid
+          animateRows={true}
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          autoGroupColumnDef={autoGroupColumnDef}
+          rowGroupPanelShow="always"
+          enableRangeSelection={true}
+          enableCharts={true}
+          rowData={rowData()}
+          rowSelection="multiple"
+          onFirstDataRendered={onFirstDataRendered}
+          groupSelectsChildren={true}
+          suppressRowClickSelection={true}
+          ref={gridRef!}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
